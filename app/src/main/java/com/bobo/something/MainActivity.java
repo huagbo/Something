6a154ba89a8f6da2e71af3e27f33f6f8e4f09cc0
huagbo <596911544@qq.com>
2017-06-21 10:21:25 +0800

    功能添加

--- app/src/main/java/com/bobo/something/MainActivity.java
+++ app/src/main/java/com/bobo/something/MainActivity.java
@@ -1,11 +1,8 @@
 package com.bobo.something;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.bobo.something.base.BaseActivity;
-import com.bobo.something.view.Flow;
 
 
 /**
@@ -23,20 +20,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void init(Bundle savedInstanceState) {
         addNavTitle("首页");
-
-        ViewGroup.LayoutParams layoutParams= new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        Flow flowLo= new Flow(this);
-        flowLo.addView(new Button(this),layoutParams);
-        flowLo.addView(new Button(this),layoutParams);
-        flowLo.addView(new Button(this),layoutParams);
-        flowLo.addView(new Button(this),layoutParams);
-        flowLo.addView(new Button(this),layoutParams);
-
-        contentView.addView(flowLo);
-
-
-
-
 
     }
 }
